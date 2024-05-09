eab670edf4b0257cb61a02bb319e9969d75c39cb
Gonzalo_MA <gonzalo.mesas.aranda@gmail.com>
2024-05-09 21:22:50 +0200

    Correccion de la funcion de actualizacion

--- secuencial.py
+++ secuencial.py
@@ -6,24 +6,23 @@ from particula import Particula
 
 # Parametros
 tam_pantalla = 10
-tam_punto = 4
-n = 20 # numero de particulas
+tam_punto = 3
+n = 50 # numero de particulas
 r_max = 5
 beta = 0.3 # Corte con el eje x de la funcion de fuerza
-mu = 0.8 # Rozamiento
-dt = 0.1 
+mu = 0.7 # Rozamiento
+dt = 0.05
+force_factor = 1
 
 # Matriz de atraccion
-matriz_atraccion = np.array([[1, 0.3, -0.6, -0.8],
-                             [0.3, -0.5, 0.8, -0.5],
-                             [-0.6, 0.8, 1, 0.1],
-                             [-0.8, -0.5, 0.1, 0.2]])
+def generar_matriz_aleatoria():
+    matriz = np.random.uniform(-1, 1, size=(4, 4))
+    matriz_simetrica = (matriz + matriz.T) / 2
+    filas_columnas = ['r', 'lime', 'c', 'y']
+    matriz_atraccion = pd.DataFrame(matriz_simetrica, index=filas_columnas, columns=filas_columnas)
+    return matriz_atraccion
 
-filas_columnas = ['r', 'lime', 'c', 'y']
-
-matriz_atraccion = pd.DataFrame(matriz_atraccion, index=filas_columnas, columns=filas_columnas)
-
-
+matriz_atraccion = generar_matriz_aleatoria()
 
 
 # Pantalla
@@ -49,29 +48,41 @@ def init():
         ax.plot(particula.posicionX, particula.posicionY, marker='o', color=particula.color, markersize=tam_punto)
     return []
 
-def mover(particula):
-    posX = particula.posicionX
-    posY = particula.posicionY
-    for p in particulas:
-        distX, distY, distancia = particula.distancia(p)
-        distX *= 1/r_max
-        distY *= 1/r_max
-        distancia *= 1/r_max
-        if distancia != 0 and distancia < 1:
-            if distancia < beta:
-                particula.velocidadX = mu*particula.velocidadX + ((distX/beta)-1)*dt*r_max
-                particula.velocidadY = mu*particula.velocidadY + ((distY/beta)-1)*dt*r_max
-                posX = posX + ((distX/beta)-1)*dt*dt*r_max
-                posY = posY + ((distY/beta)-1)*dt*dt*r_max
-            else:
-                atraccion = matriz_atraccion.at[particula.color, p.color]
-                particula.velocidadX = mu*particula.velocidadX + (atraccion*(1-(abs(2*distX-1-beta)/(1-beta))))*dt*r_max
-                particula.velocidadY = mu*particula.velocidadY + (atraccion*(1-(abs(2*distY-1-beta)/(1-beta))))*dt*r_max
-                posX = posX + (atraccion*(1-(abs(2*distX-1-beta)/(1-beta))))*dt*dt*r_max
-                posY = posY + (atraccion*(1-(abs(2*distY-1-beta)/(1-beta))))*dt*dt*r_max
-    particula.posicionX = posX
-    particula.posicionY = posY
+def fuerza(r, a):
+    if r < beta:
+        return r/beta - 1
+    elif beta < r and r < 1:
+        return a*(1-(abs(2*r -1 -beta))/(1-beta))
+    else:
+        return 0
 
+def actualizar_velocidad(n_particula):
+    totalForceX = 0
+    totalForceY = 0
+    particula = particulas[n_particula]
+
+    for j in range(len(particulas)):
+        if n_particula == j: continue
+        rX, rY, r = particula.distancia(particulas[j])
+        if r > 0.05 and r < r_max:
+            atraccion = matriz_atraccion.at[particula.color, particulas[j].color]
+            #fuerza = atraccion/r
+            f = fuerza(r/r_max, atraccion)
+            totalForceX += rX / r * f
+            totalForceY += rY / r * f
+    
+    totalForceX *= r_max*force_factor
+    totalForceY *= r_max*force_factor
+
+    particula.velocidadX *= mu
+    particula.velocidadY *= mu
+
+    particula.velocidadX += totalForceX*dt
+    particula.velocidadY += totalForceY*dt
+
+def actualizar_posicion(n_particula):
+    particulas[n_particula].posicionX += particulas[n_particula].velocidadX * dt
+    particulas[n_particula].posicionY += particulas[n_particula].velocidadY * dt
 
 # Función de actualización para animar el punto
 def update(frame):
@@ -79,9 +90,10 @@ def update(frame):
     ax.axis('off')
     ax.set_xlim(0, tam_pantalla)
     ax.set_ylim(0, tam_pantalla)
-    for particula in particulas:
-        mover(particula)
-        ax.plot(particula.posicionX, particula.posicionY, marker='o', color=particula.color, markersize=tam_punto)
+    for n_particula in range(len(particulas)):
+        actualizar_velocidad(n_particula)
+        actualizar_posicion(n_particula)
+        ax.plot(particulas[n_particula].posicionX, particulas[n_particula].posicionY, marker='o', color=particulas[n_particula].color, markersize=tam_punto)
 
     return []
 
